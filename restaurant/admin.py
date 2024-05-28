3c38bdec6e0740f3524acb20271f44fc073b8930
Igor <104066443+OldXrist@users.noreply.github.com>
2024-05-28 03:38:51 +0300

    Created Order and OrderDetail

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Menu, CartItem, Booking
+from .models import Menu, CartItem, Booking, Order, OrderDetail
 
 
 # Register your models here.
 admin.site.register(Menu)
 admin.site.register(Booking)
 admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderDetail)
--- restaurant/models.py
+++ restaurant/models.py
@@ -32,3 +32,22 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f'{self.quantity} x {self.product.name}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total = models.PositiveIntegerField(default=0)
+    address = models.TextField(max_length=100, default='')
+
+    def __str__(self):
+        return f'Order #{self.id}'
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'Order #{self.order.id}, {self.product.name} x{self.quantity}'
