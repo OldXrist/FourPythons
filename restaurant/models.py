412d5ccc51812eba893012720328af5c45ee4288
Igor <104066443+OldXrist@users.noreply.github.com>
2024-06-12 17:50:48 +0300

    Update models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -10,6 +10,10 @@ class Booking(models.Model):
     guest_number = models.IntegerField('Количество гостей')
     comment = models.CharField('Комментарий', max_length=1000)
 
+    class Meta:
+        verbose_name = 'Бронирование'
+        verbose_name_plural = 'Бронирования'
+
     def __str__(self):
         return self.full_name
 
@@ -19,6 +23,10 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(null=False)
     menu_item_description = models.TextField(max_length=1000, default='')
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
 
     def __str__(self):
         return self.name
@@ -30,6 +38,10 @@ class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзины'
+
     def __str__(self):
         return f'{self.quantity} x {self.product.name}'
 
@@ -40,6 +52,10 @@ class Order(models.Model):
     total = models.PositiveIntegerField(default=0)
     address = models.TextField(max_length=100, default='')
 
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
     def __str__(self):
         return f'Order #{self.id}'
 
@@ -48,6 +64,10 @@ class OrderDetail(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Menu, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Деталь заказа'
+        verbose_name_plural = 'Детали заказов'
 
     def __str__(self):
         return f'Order #{self.order.id}, {self.product.name} x{self.quantity}'
@@ -58,6 +78,10 @@ class Review(models.Model):
     review_date = models.DateTimeField(auto_now_add=True)
     review = models.TextField(max_length=500)
     rating = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
     def __str__(self):
         return f'{User.username}'
